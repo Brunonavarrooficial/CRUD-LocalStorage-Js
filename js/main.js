7719727f53f2bed83ec6f217ed79a7abe5c25516
Brunonavarrooficial <bnacessoria@gmail.com>
2022-11-02 22:52:38 -0300

    Script Modal

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active');
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active');
+const closeModal = () => {
+    document.getElementById('modal').classList.remove('active');
+    clearFields();
+}
+
 
 const tempClient = {
     nome: 'nicolas',
@@ -18,20 +21,51 @@ const setLocalStorage = (dbClient) => localStorage.setItem('db_client', JSON.str
 
 // CREATE
 const createClient = (client) => {
-    const dbClient =  getLocalStorage();    
+    const dbClient = getLocalStorage();
     dbClient.push(client);
-    setLocalStorage(dbClient);    
+    setLocalStorage(dbClient);
 }
 // READ
-const readClient = () =>  getLocalStorage();
+const readClient = () => getLocalStorage();
 
 // UPDATE
 const updateClient = (index, client) => {
     const dbClientUp = readClient();
     dbClientUp[index] = client;
     setLocalStorage(dbClientUp);
+}
 
-} 
+// DELETE
+const deleteClient = (index) => {
+    const dbClientDel = readClient();
+    dbClientDel.splice(index, 1);
+    setLocalStorage(dbClientDel);
+}
+
+// Interação com layout HTML
+
+const isValidFields = () => {
+    return document.getElementById('form').reportValidity();
+}
+
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field');
+    fields.forEach(field => field.value = '');
+}
+
+const saveClient = () => {
+    if (isValidFields()) {
+        const newUser = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular: document.getElementById('celular').value,
+            cidade: document.getElementById('cidade').value,
+        }
+        createClient(newUser);
+        closeModal();
+        console.log('Cliente cadstrado com sucesso!');
+    }
+}
 
 // Eventos
 
@@ -39,4 +73,10 @@ document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
+    .addEventListener('click', closeModal);
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient);
+
+document.getElementById('cancelar')
     .addEventListener('click', closeModal);
