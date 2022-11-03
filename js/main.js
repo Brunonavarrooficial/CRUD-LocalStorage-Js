9575be99302960bbf536223e7856571c53244768
Brunonavarrooficial <bnacessoria@gmail.com>
2022-11-02 21:38:54 -0300

    Script Init

--- js/main.js
+++ js/main.js
@@ -4,7 +4,34 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active');
 
+const tempClient = {
+    nome: 'nicolas',
+    email: 'nicolasnavarrooficial@gmail.com',
+    celular: '(11)95999-6260',
+    cidade: 'são paulo',
+}
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+
+const setLocalStorage = (dbClient) => localStorage.setItem('db_client', JSON.stringify(dbClient));
+
 // CRUD - create read update delete
+
+// CREATE
+const createClient = (client) => {
+    const dbClient =  getLocalStorage();    
+    dbClient.push(client);
+    setLocalStorage(dbClient);    
+}
+// READ
+const readClient = () =>  getLocalStorage();
+
+// UPDATE
+const updateClient = (index, client) => {
+    const dbClientUp = readClient();
+    dbClientUp[index] = client;
+    setLocalStorage(dbClientUp);
+
+} 
 
 // Eventos
 
