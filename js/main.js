b15e67a466b93ccbbf8c02d6b3e5a67606913191
Brunonavarrooficial <bnacessoria@gmail.com>
2022-11-04 23:21:01 -0300

    Scrpit Edit Modal

--- js/main.js
+++ js/main.js
@@ -54,21 +54,31 @@ const saveClient = () => {
             celular: document.getElementById('celular').value,
             cidade: document.getElementById('cidade').value,
         }
-        createClient(newUser);
-        closeModal();
-        console.log('Cliente cadstrado com sucesso!');
+        const index = document.getElementById('nome').dataset.index;
+        if (index == 'new') {
+            createClient(newUser);
+            updateTable();
+            closeModal();
+            console.log('Cliente cadastrado com sucesso!');
+        }else{
+            updateClient(index, newUser);
+            updateTable();
+            closeModal();
+            console.log('Cliente editado com sucesso!');
+        }
+
     }
 }
 
-const createRow = (client) => {
+const createRow = (client, index) => {
     const newRow = document.createElement('tr');
     newRow.innerHTML = `<td>${client.nome}</td>
     <td>${client.email}</td>
     <td>${client.celular}</td>
     <td>${client.cidade}</td>
     <td>
-        <button type="button" class="button green">Editar</button>
-        <button type="button" class="button red">Excluir</button>
+        <button type="button" class="button green" id="edit-${index}">Editar</button>
+        <button type="button" class="button red" id="delete-${index}">Excluir</button>
     </td>`;
     document.querySelector('#tableClient>tbody').appendChild(newRow);
 }
@@ -80,8 +90,36 @@ const clearTable = () => {
 
 const updateTable = () => {
     const dbClientTable = readClient()
-    clearTable();    
-    dbClientTable.forEach(createRow);    
+    clearTable();
+    dbClientTable.forEach(createRow);
+}
+
+const fillFields = (client) => {
+    document.getElementById('nome').value = client.nome
+    document.getElementById('email').value = client.email
+    document.getElementById('celular').value = client.celular
+    document.getElementById('cidade').value = client.cidade
+    document.getElementById('nome').dataset.index = client.index
+}
+
+const editClient = (index) => {
+    const client = readClient()[index];
+    client.index = index;
+    fillFields(client);
+    openModal();
+}
+
+const editDelete = (event) => {
+    if (event.target.type == 'button') {
+        const [action, index] = event.target.id.split('-');
+
+        if (action == 'edit') {
+            editClient(index)
+        } else {
+            console.log('deletando cliente');
+        }
+    }
+
 }
 
 updateTable();
@@ -99,3 +137,6 @@ document.getElementById('salvar')
 
 document.getElementById('cancelar')
     .addEventListener('click', closeModal);
+
+document.querySelector('#tableClient>tbody')
+    .addEventListener('click', editDelete);
